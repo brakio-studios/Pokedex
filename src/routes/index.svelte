5f6bf733001b3815b49488617278b7fb5a630bf1
Bernardo Montoya <ber.montoya12@gmail.com>
2022-02-10 19:38:04 -0600

    feat: add search functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,20 @@
 	import PokemonCard from '../components/PokemonCard.svelte';
 	import { pokemon, fetchPokemon } from '../store/pokestore';
 
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter((pokemon) => {
+				const included = pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
+				return included;
+			});
+		} else {
+			filteredPokemon = [...$pokemon];
+		}
+	}
+
 	fetchPokemon();
 </script>
 
@@ -12,11 +26,12 @@
 <div class="container mx-auto">
 	<h1 class="text-4xl text-center my-8 uppercase">Pokedex with SvelteKit</h1>
 	<input
+		bind:value={searchTerm}
 		class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
 		placeholder="Search Pokemon"
 	/>
 	<div class="py-4 grid gap-4 md:grid-cols-3 grid-cols-1">
-		{#each $pokemon as poke}
+		{#each filteredPokemon as poke}
 			<PokemonCard pokemon={poke} />
 		{/each}
 	</div>
